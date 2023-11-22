f36b34c8968cde4ab997284645941ec7ac9db651
Sabelodone <sabelozondo825gmail.com>
2023-11-22 04:46:17 -0800

    a script that prepares a MySQL server for the project:

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,47 +1,12 @@
-import mysql.connector
+# Script to prepare a MySQL server for the project:
+ -- Creates a database 'hbnb_dev_db'
+ -- Creates a new user 'hbnb_dev' in localhost with the password 'hbnb_dev_pwd'
+ -- Grants all privileges on 'hbnb_dev_db' to 'hbnb_dev'@'localhost'
+ -- Grants SELECT privilege on 'performance_schema' to 'hbnb_dev'@'localhost'
+ -- If the database hbnb_dev_db or the user hbnb_dev already exists, the script should not fail
 
-def prepare_mysql_server():
-    try:
-        # Establish connection to the MySQL server
-        connection = mysql.connector.connect(
-            host='localhost',
-            user='root',  # Replace with your MySQL username
-            password='your_password'  # Replace with your MySQL password
-        )
+ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+ GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+ GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 
-        # Create a cursor object to execute SQL queries
-        cursor = connection.cursor()
-
-        # Explanation: Prepare the MySQL server for the project
-        # Create the database if it doesn't already exist
-        # Create a user and grant privileges on the database
-        # Grant select privileges on performance_schema
-        # Flush privileges to apply changes
-        sql_queries = """
-            CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-            CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-            GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-            GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-            FLUSH PRIVILEGES;
-        """
-
-        # Execute the SQL statements
-        cursor.execute(sql_queries, multi=True)
-
-        # Commit the changes to the database
-        connection.commit()
-
-        print("MySQL server prepared successfully.")
-
-    except mysql.connector.Error as error:
-        print(f"Error while preparing MySQL server: {error}")
-
-    finally:
-        # Close cursor and connection
-        if 'cursor' in locals():
-            cursor.close()
-        if 'connection' in locals() and connection.is_connected():
-            connection.close()
-
-# Call the function to prepare the MySQL server
-prepare_mysql_server()
