d5ec9be048066700a79f4aaca2cb202ed0419043
Sabelodone <sabelozondo825gmail.com>
2024-01-04 02:18:38 -0800

    commit

--- models/place.py
+++ models/place.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Table, Column, Integer, ForeignKey
 
 association_table = Table("place_amenity", Base.metadata,
                           Column("place_id", String(60),
